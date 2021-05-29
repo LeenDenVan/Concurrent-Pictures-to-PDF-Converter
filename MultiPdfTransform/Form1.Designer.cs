68f938d7b5bb69b742d094249a77f9aaa7cb2c0c
LeenDenVan <1546596533@qq.com>
2021-05-30 01:15:07 +0800

    Create

---
+++ MultiPdfTransform/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MultiPdfTransform
+{
+    partial class Images2PDFs
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            this.openFileDialog1.Title = "Choose a image file.";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.16327F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.83673F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.progressBar1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.05051F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.94949F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(735, 495);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(210, 325);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(522, 52);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(522, 316);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0/0";
+            // 
+            // Images2PDFs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 495);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Images2PDFs";
+            this.Text = "Images2PDFs";
+            this.Load += new System.EventHandler(this.Images2PDFs_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MultiPdfTransform/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
+
+namespace MultiPdfTransform
+{
+    public partial class Images2PDFs : Form
+    {
+        private Queue<string> currentImages = new Queue<string>();
+        IEnumerable<PaperSize> paper;
+        
+        public Images2PDFs()
+        {
+            InitializeComponent();
+            updateText = new UpdateText(UpdateTextMet);
+            initializeProgress = new InitializeProgressBar(InitializeProgress);
+            progressBar1.Value = 0;
+            progressBar1.Step = 1;
+            progressBar1.Visible = true;
+            progressBar1.Minimum = 0;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public delegate void UpdateText(string msg);
+        public UpdateText updateText;
+        public delegate void InitializeProgressBar(int Maximum);
+        public InitializeProgressBar initializeProgress;
+        public void InitializeProgress(int Maximum)
+        {
+            progressBar1.Maximum = Maximum;
+        }
+        public void UpdateTextMet(string msg)
+        {
+            textBox1.AppendText(msg + "\r\n");
+            textBox1.ScrollToCaret();
+        }
+        public void ThreadText(string path)
+        {
+            this.BeginInvoke(updateText, path);
+        }
+        public void ThreadInitProgress(int Maximum)
+        {
+            this.Invoke(initializeProgress, Maximum);
+        }
+
+
+        public void ExecuteIt(string path)
+        {
+            ThreadText("Start Processing:"+path);
+            List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+            int totalNum = dirs.Count();
+            Console.WriteLine(totalNum);
+            ThreadInitProgress(totalNum);
+            /*dirs.Sort(delegate(string a, string b) {
+                int i = Directory.EnumerateFiles(a).Count();
+                int j = Directory.EnumerateFiles(b).Count();
+                if (i < j) return 1;
+                else if (i == j) return 0;
+                else return -1;
+            });*/
+            int passedPages = 0;
+            this.Invoke(new Action(() => { label1.Text = passedPages + "/" + totalNum; }));
+            Parallel.ForEach(dirs, new ParallelOptions { MaxDegreeOfParallelism = 30 } , dir =>
+            {
+                string dirt = dir.Insert(dir.LastIndexOf('\\'), "Out");
+                //Console.WriteLine("Executing:" + Directory.EnumerateFiles(dir).Count() + " " + passedPages + "," + dir);
+                passedPages++;
+                this.Invoke(new Action(() => { progressBar1.PerformStep(); }));
+                if (File.Exists(dirt + ".pdf"))
+                {
+                    ThreadText("Passed "+passedPages+"with :" + dir.Substring(dir.LastIndexOf('\\')));
+                    this.Invoke(new Action(()=> { label1.Text = passedPages + "/" + totalNum; }));
+                    return;
+                }
+                //thistimePages++;
+                Queue<string> current = new Queue<string>(Directory.EnumerateFiles(dir));
+                Console.WriteLine(dirt);
+                ThreadText("Processing Id="+passedPages+"with "+current.Count+"pages:" + dir.Substring(dir.LastIndexOf('\\')));
+                printSinglePDF(dirt + ".pdf", current);
+                this.Invoke(new Action(() => { label1.Text = passedPages + "/" + totalNum; }));
+                return;
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.TopMost = true;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Clear();
+                progressBar1.Value = 0;
+                ThreadPool.QueueUserWorkItem(w =>
+                {
+                    ExecuteIt(folderBrowserDialog1.SelectedPath);
+                    this.Invoke(new MethodInvoker(() => MessageBox.Show("Done")));
+                },null);
+            }
+        }
+
+        private Boolean isIllegal(string p)
+        {
+            return !p.EndsWith(".jpg") && !p.EndsWith(".png") && !p.EndsWith(".gif");
+        }
+
+        private void printSinglePDF(String filename, Queue<string> que)
+        {
+            Queue<string> pkf = que;
+            try
+            {
+                using (PrintDocument pdo = new PrintDocument())
+                {
+                    pdo.PrinterSettings.PrinterName = "Microsoft Print to PDF";
+                    pdo.PrinterSettings.PrintToFile = true;
+                    pdo.PrinterSettings.PrintFileName = filename;
+                    //pdo.PrintController = new StandardPrintController();
+                    paper = pdo.PrinterSettings.PaperSizes.Cast<PaperSize>();
+                    pdo.PrintPage += new PrintPageEventHandler((object sender, PrintPageEventArgs e) =>
+                    {
+                        string p = pkf.Dequeue();
+                        while (isIllegal(p) && pkf.Count > 0) p = pkf.Dequeue();
+                        if (pkf.Count > 0)
+                        {
+                            try
+                            {
+                                using (Image img = Image.FromFile(p))
+                                {
+                                    int wid = paper.First().Width;
+                                    int hei = img.Height * paper.First().Width / img.Width;
+                                    int hei2 = hei > paper.First().Height ? paper.First().Height : hei;
+                                    if ((float)hei2 / paper.First().Height > 0.85f) hei2 = paper.First().Height;
+                                    if ((float)wid / paper.First().Width > 0.85f) wid = paper.First().Width;
+                                    if (img != null)
+                                    {
+                                        using (Image timg = resizeImage(img, new Size(wid, hei2)))
+                                        {
+
+                                            img.Dispose();
+                                            e.Graphics.DrawImage(timg, new Point(0, 0));
+                                            timg.Dispose();
+                                        }
+                                    }
+                                    else
+                                    {
+
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Broken Image:" + p);
+                            }
+                        }
+                        e.HasMorePages = pkf.Count != 0;
+                    });
+                    pdo.Print();
+                    pdo.Dispose();
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Error Happened in:" + filename +"\n"+ ex.StackTrace);
+            }
+        }
+
+        private void pdoc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            string p = currentImages.Dequeue();
+            while (isIllegal(p)) p = currentImages.Dequeue();
+            using (Image img = Image.FromFile(p))
+            {
+                int wid = paper.First().Width;
+                int hei = img.Height * paper.First().Width / img.Width;
+                int hei2 = hei > paper.First().Height ? paper.First().Height : hei;
+                if ((float)hei2 / paper.First().Height > 0.85f) hei2 = paper.First().Height;
+                if ((float)wid / paper.First().Width > 0.85f) wid = paper.First().Width;
+                using (Image temp = resizeImage(img, new Size(wid, hei2)))
+                {
+                    e.Graphics.DrawImage(temp, new Point(0, 0));
+                }
+            }
+            e.HasMorePages = currentImages.Count != 0;
+        }
+
+        private static System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+            //Get the image current width  
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height  
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //Calulate  width with new desired size  
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //Calculate height with new desired size  
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //New Width  
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height  
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height  
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+        }
+
+        private void Images2PDFs_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
